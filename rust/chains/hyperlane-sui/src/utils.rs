56565c26e90c15834646f08a95265c4f7dec6cb2
Richard Melkonian <r.v.melkonian@gmail.com>
2024-01-24 17:01:50 +0000

    tweaks

--- rust/chains/hyperlane-sui/src/utils.rs
+++ rust/chains/hyperlane-sui/src/utils.rs
@@ -1,8 +1,16 @@
 use crate::{AddressFormatter, SuiRpcClient, TxSpecificData};
-use hyperlane_core::{ChainCommunicationError, ChainResult, LogMeta, H256, H512, U256};
+use hyperlane_core::{
+    ChainCommunicationError, ChainResult, InterchainGasPayment, LogMeta, H256, H512, U256,
+};
 use solana_sdk::account;
-use sui_sdk::{types::{base_types::{ObjectID, SuiAddress}, digests::TransactionDigest}, rpc_types::{EventFilter, SuiEvent}};
 use std::{ops::RangeInclusive, str::FromStr};
+use sui_sdk::{
+    rpc_types::{EventFilter, SuiEvent},
+    types::{
+        base_types::{ObjectID, SuiAddress},
+        digests::TransactionDigest,
+    },
+};
 
 /// Convert address string to H256
 pub fn convert_hex_string_to_h256(addr: &str) -> Result<H256, String> {
@@ -10,14 +18,12 @@ pub fn convert_hex_string_to_h256(addr: &str) -> Result<H256, String> {
     H256::from_str(&formated_addr).map_err(|e| e.to_string())
 }
 
-pub async fn get_filtered_events<T>(
+pub async fn get_filtered_events(
     sui_client: &SuiRpcClient,
     package_id: &Option<ObjectID>,
     struct_tag: &str,
-    range: RangeInclusive<u64>,
-) -> ChainResult<Vec<(T, LogMeta)>> 
-{
-
+    range: RangeInclusive<u32>,
+) -> ChainResult<Vec<(InterchainGasPayment, LogMeta)>> {
     if package_id.is_none() {
         return Err(ChainCommunicationError::SuiObjectReadError(
             "Package ID is None".to_string(),
@@ -25,12 +31,7 @@ pub async fn get_filtered_events<T>(
     }
     let events_page = sui_client
         .event_api()
-        .query_events(
-            EventFilter::Package(package_id.unwrap()),
-            None,
-            None,
-            true,
-        )
+        .query_events(EventFilter::Package(package_id.unwrap()), None, None, true)
         .await
         .map_err(|e| {
             ChainCommunicationError::SuiObjectReadError(format!(
@@ -41,21 +42,19 @@ pub async fn get_filtered_events<T>(
 
     //TODO: LogMeta will need to be an enum to handle different chains
     //the data its expecting here doesn't make much sense for Sui.
-    let parsed_events: Vec<(T, LogMeta)> = events_page
-        .data
-        .into_iter()
-        .map(|event| {
-            // Mainly using dummy values untile LogMeta is an enum
-            let log_meta = LogMeta {
-                address: event.sender.to_bytes().into(), // Should this be the sender? 
-                block_number:  0, // No block numbers in Sui
-                block_hash: H256::zero(), // No block hash in Sui
-                transaction_id: H512::zero(),  
-                transaction_index: 0, // Not sure what this val should be,
-                log_index: U256::zero(), // No block structure in Sui 
-            };
-            (event, log_meta)
-        })
-        .collect();
-    Ok(parsed_events)
+    let mut messages: Vec<(InterchainGasPayment, LogMeta)> = Vec::with_capacity((range.end() - range.start()) as usize);
+    for event in events_page.data.into_iter() {
+        //let json_data = event.parsed_json.clone();
+        // Mainly using dummy values untile LogMeta is an enum
+        let log_meta = LogMeta {
+            address: event.sender.to_bytes().into(), // Should this be the sender?
+            block_number: 0,                         // No block numbers in Sui
+            block_hash: H256::zero(),                // No block hash in Sui
+            transaction_id: H512::zero(),
+            transaction_index: 0,    // Not sure what this val should be,
+            log_index: U256::zero(), // No block structure in Sui
+        };
+        messages.push((event.parsed_json.try_into()?, log_meta));
+    }
+    Ok(messages)
 }
