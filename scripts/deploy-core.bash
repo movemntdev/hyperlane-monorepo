83f04fe97e95ab3dcca69e17a1701d7f0cfbbacd
Richard Melkonian <r.v.melkonian@gmail.com>
2024-06-02 19:10:21 +0200

    update script

--- scripts/deploy-core.bash
+++ scripts/deploy-core.bash
@@ -1,11 +1,12 @@
 #!/bin/bash -e
 
 DEPLOYER_KEY=$(jq -r '.private_keys[0]' "./process-compose/anvil.json")
+MULTISIG_CONFIG_FILE="./configs/ism.yaml"
 
-hyperlane deploy core \
-    --targets anvil1,aptoslocalnet1 \ # all the chains you want to bridge between
-    --ism $MULTISIG_CONFIG_FILE \ # path to ism.yaml config e.g. ./configs/ism.yaml
-    --key $DEPLOYER_KEY \ # (optional) your private key to pay for transactions; can also be provided via HYP_KEY env variable
-    --registry \ ./configs
-    --overrides # (optional) path to a override registry; defaults to the local ./ path
+npx hyperlane deploy core \
+  --targets anvil,aptoslocalnet1 \
+  --ism $MULTISIG_CONFIG_FILE \
+  --key $DEPLOYER_KEY \
+  --registry ./configs \
+  --overrides " " \
 
